3445b19f2cd02f2d2cb13ecd846e132a4b22119b
JiaDingYi <wzy2010416033@163.com>
2024-05-07 17:28:00 +0800

    add pm2 config

---
+++ pm2.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+        {
+            name: 'myapp',
+            instances: 'max',
+            script: 'bin/www',
+            exec_mode: 'cluster',
+            watch: false,
+            autorestart: true,
+            env: {
+                DEBUG: 'myapp:*',
+                PORT: 80,
+            }
+        }
+    ]
+};
